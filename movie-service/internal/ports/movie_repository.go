fe84fff47d98791e92148631ba1dc0349b8632ff
Luiz David Bastos Benevides <luizdavidx798@gmail.com>
2026-07-08 19:38:46 -0400

    camdaa principal finalizada ainda sem testes

--- movie-service/internal/ports/movie_repository.go
+++ movie-service/internal/ports/movie_repository.go
@@ -1,2 +1,14 @@
 package ports
 
+import (
+	"context"
+
+	"github.com/luizdavid/movies-challenge/movie-service/internal/domain"
+)
+
+type MovieRepository interface {
+	FindAll(ctx context.Context) ([]domain.Movie, error)
+	FindByID(ctx context.Context, id int64) (*domain.Movie, error)
+	Create(ctx context.Context, movie domain.Movie) (*domain.Movie, error)
+	Delete(ctx context.Context, id int64) error
+}
