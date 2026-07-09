ff5a288950e72daff8ed4fa7998d3eeda17db896
Luiz David Bastos Benevides <luizdavidx798@gmail.com>
2026-07-09 00:01:16 -0400

    add id por requisição para estruturar logs

--- api-gateway/internal/middleware/logger.go
+++ api-gateway/internal/middleware/logger.go
@@ -1,1 +1,27 @@
 package middleware
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+func Logger(appLogger *zap.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+
+		c.Next()
+
+		latency := time.Since(start)
+
+		appLogger.Info("http request completed",
+			zap.String("request_id", GetRequestID(c)),
+			zap.String("method", c.Request.Method),
+			zap.String("path", c.Request.URL.Path),
+			zap.Int("status", c.Writer.Status()),
+			zap.Duration("latency", latency),
+			zap.String("client_ip", c.ClientIP()),
+		)
+	}
+}
